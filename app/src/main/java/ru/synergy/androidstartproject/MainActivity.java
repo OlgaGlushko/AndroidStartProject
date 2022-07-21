2a7878415a3cf0003871b975b1170b2678ed68d7
OlgaGlushko <agnia_1987@bk.rugit config --global user.email agnia_1987@bk.ru>
2022-07-21 16:51:36 +0700

    We have learned LinearLayout2

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -3,6 +3,7 @@ package ru.synergy.androidstartproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.GridLayout;
 import android.widget.TextView;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+TextView textView = (TextView) findViewById(R.id.normal);
+
+TextView.setText("Text from Java code");
+TextView.setTextColor(Color.RED);
 
      //   TextView textView  = (TextView) findViewById(R.id.header);
      //   textView.setText("Hello from Java");
