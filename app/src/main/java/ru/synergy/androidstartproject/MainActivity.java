a44959e894c834711668510f6559febcc2082793
OlgaGlushko <agnia_1987@bk.rugit config --global user.email agnia_1987@bk.ru>
2022-07-22 12:30:03 +0700

    my project

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -13,39 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-TextView textView = (TextView) findViewById(R.id.normal);
+//TextView textView = (TextView) findViewById(R.id.normal);
 
-TextView.setText("Text from Java code");
-TextView.setTextColor(Color.RED);
-
-     //   TextView textView  = (TextView) findViewById(R.id.header);
-     //   textView.setText("Hello from Java");
-
-
- //       ConstraintLayout constraintLayout = new ConstraintLayout(this);
-
-
- //       TextView textView = new TextView(this);
-   //     textView.setText("Hello Android!");
-     //   textView.setTextSize(26);
-
-       // ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-         //       ConstraintLayout.LayoutParams.WRAP_CONTENT,
-           //     ConstraintLayout.LayoutParams.WRAP_CONTENT);
-
-        //layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        //layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        //layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-
-
-        //textView.setLayoutParams(layoutParams);
-        //constraintLayout.addView(textView);
-
-
-        //setContentView(constraintLayout);
-
+//TextView.setText("Text from Java code");
+//TextView.setTextColor(Color.RED);
 
 
 
